9cc5f566a138c1436719a71f67104ef4d3031bb1
Kyo <hyosing92@gmail.com>
2023-05-03 15:31:16 +0900

    feat : SceneCoordinator프로토콜 정의 및 DefaultSceneCoordinator 구현하여 View 이동 및 닫기 구현

---
+++ ReceiptManager/ReceiptManager/Source/Scene/DefaultSceneCoordinator.swift
@@ -0,0 +1,94 @@
+//
+//  DefaultSceneCoordinator.swift
+//  ReceiptManager
+//
+//  Created by parkhyo on 2023/05/02.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+// MARK: - Extension UIViewController
+// 실제로 보여지는 ViewController
+extension UIViewController {
+    var sceneViewController: UIViewController {
+        return children.last ?? self
+    }
+}
+
+final class DefaultSceneCoordinator: SceneCoordinator {
+    private let disposeBag = DisposeBag()
+    
+    private var window: UIWindow
+    private var currentViewController: UIViewController
+    
+    required init(window: UIWindow) {
+        self.window = window
+        currentViewController = window.rootViewController!
+    }
+    
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool) -> Completable {
+        let subject = PublishSubject<Never>()
+        let moveTarget = scene.instantiate()
+        
+        switch style {
+        case .root:
+            currentViewController = moveTarget.sceneViewController
+            window.rootViewController = moveTarget
+            subject.onCompleted()
+        case .push:
+            guard let navigation = currentViewController.navigationController else {
+                subject.onError(TransitionError.navigationControllerMissing)
+                break
+            }
+            
+            navigation.rx.willShow
+                .withUnretained(self)
+                .subscribe(onNext: { coordinator, event in
+                    coordinator.currentViewController = event.viewController.sceneViewController
+                })
+                .disposed(by: disposeBag)
+            
+            navigation.pushViewController(moveTarget, animated: true)
+            currentViewController = moveTarget.sceneViewController
+            
+            subject.onCompleted()
+        case .modal:
+            currentViewController.present(moveTarget, animated: animated) {
+                subject.onCompleted()
+            }
+            
+            currentViewController = moveTarget.sceneViewController
+        }
+        
+        return subject.asCompletable()
+    }
+    
+    func close(animated: Bool) -> Completable {
+        
+        return Completable.create { [weak self] completable in
+            
+            // Modal 방식 인 경우
+            if let presentingViewController = self?.currentViewController.presentingViewController {
+                self?.currentViewController.dismiss(animated: animated) {
+                    self?.currentViewController = presentingViewController.sceneViewController
+                    completable(.completed)
+                }
+                // Push 방식 인 경우
+            } else if let navigation = self?.currentViewController.navigationController {
+                guard navigation.popViewController(animated: animated) != nil else {
+                    completable(.error(TransitionError.cannotPop))
+                    return Disposables.create()
+                }
+                
+                self?.currentViewController = navigation.viewControllers.last!.sceneViewController
+                completable(.completed)
+            } else {
+                completable(.error(TransitionError.unknown))
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
