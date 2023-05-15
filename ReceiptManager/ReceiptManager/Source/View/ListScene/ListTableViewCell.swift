382442458726a800e50b85f0a5986d0e9cb064f4
Kyo <hyosing92@gmail.com>
2023-05-15 15:06:04 +0900

    feat : ListTableViewCell 오토레이아웃 조정

--- ReceiptManager/ReceiptManager/Source/View/ListScene/ListTableViewCell.swift
+++ ReceiptManager/ReceiptManager/Source/View/ListScene/ListTableViewCell.swift
@@ -8,18 +8,29 @@
 import UIKit
 
 final class ListTableViewCell: UITableViewCell {
-    private let paymentImageView: UIImageView = {
+    private let payImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.tintColor = .yellow
-        imageView.contentMode = .scaleToFill
+        imageView.tintColor = ConstantColor.favoriteColor
+        imageView.contentMode = .scaleAspectFit
         imageView.image = UIImage(systemName: "wonsign.square.fill")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
     
+    private lazy var mainInfoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [productNameLabel, storeLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        
+        return stackView
+    }()
+    
     private let productNameLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .white
         label.font = .preferredFont(forTextStyle: .body)
         
         return label
@@ -27,30 +38,36 @@ final class ListTableViewCell: UITableViewCell {
     
     private let storeLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .white
         label.font = .preferredFont(forTextStyle: .body)
         
         return label
+    }()
+    
+    private lazy var subInfoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [priceLabel, detailButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        
+        return stackView
     }()
     
     private let priceLabel: UILabel = {
         let label = UILabel()
+        label.textColor = .white
         label.font = .preferredFont(forTextStyle: .body)
         
         return label
     }()
     
-    private lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [
-            paymentImageView,
-            productNameLabel,
-            storeLabel,
-            priceLabel
-        ])
-        stackView.axis = .horizontal
-        stackView.distribution = .fill
-        stackView.alignment = .leading
+    private let detailButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage.init(systemName: "chevron.right"), for: .normal)
+        button.tintColor = .gray
         
-        return stackView
+        return button
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -65,7 +82,7 @@ final class ListTableViewCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        paymentImageView.tintColor = .yellow
+        payImageView.tintColor = ConstantColor.favoriteColor
     }
     
     func setupData(data: Receipt) {
@@ -74,11 +91,11 @@ final class ListTableViewCell: UITableViewCell {
         
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
-        priceLabel.text = numberFormatter.string(from: NSNumber(value: data.price))
+        priceLabel.text = String(numberFormatter.string(from: NSNumber(value: data.price)) ?? "0") + " 원"
         
         if PayType(rawValue: data.paymentType) == .card {
-            paymentImageView.tintColor = .blue
-            paymentImageView.image = UIImage(systemName: "creditcard.fill")
+            payImageView.tintColor = ConstantColor.registerColor
+            payImageView.image = UIImage(systemName: "creditcard.fill")
         }
     }
 }
@@ -91,18 +108,31 @@ extension ListTableViewCell {
         layer.borderWidth = 1
         layer.cornerRadius = 5
         
-        [mainStackView].forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
-        [mainStackView].forEach(addSubview(_:))
+        [payImageView, mainInfoStackView, subInfoStackView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        [payImageView, mainInfoStackView, subInfoStackView].forEach(contentView.addSubview(_:))
     }
     
     private func setupConstraints() {
-        let safeArea = safeAreaLayoutGuide
+        let safeArea = contentView.safeAreaLayoutGuide
         
         NSLayoutConstraint.activate([
-            mainStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            mainStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            mainStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
-            mainStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+            
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 60),
+            
+            payImageView.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor),
+            payImageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 10),
+            payImageView.widthAnchor.constraint(equalToConstant: 50),
+            payImageView.heightAnchor.constraint(equalToConstant: 50),
+            
+            mainInfoStackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
+            mainInfoStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -10),
+            mainInfoStackView.leadingAnchor.constraint(equalTo: payImageView.trailingAnchor, constant: 10),
+            mainInfoStackView.widthAnchor.constraint(equalTo: safeArea.widthAnchor, multiplier: 0.5),
+            
+            subInfoStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -10),
+            subInfoStackView.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor)
         ])
     }
 }
