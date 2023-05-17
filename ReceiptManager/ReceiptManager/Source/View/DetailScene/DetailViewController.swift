9990e92eb00235c49271730e4576504602e12470
Kyo <hyosing92@gmail.com>
2023-05-17 21:37:40 +0900

    feat : DetailView 세부 구현

--- ReceiptManager/ReceiptManager/Source/View/DetailScene/DetailViewController.swift
+++ ReceiptManager/ReceiptManager/Source/View/DetailScene/DetailViewController.swift
@@ -14,29 +14,34 @@ final class DetailViewController: UIViewController, ViewModelBindable {
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        layout.minimumLineSpacing = 10
+        
         let collectionCellWidth = UIScreen.main.bounds.width * 0.7
         
-        layout.itemSize  = CGSize(width: collectionCellWidth, height: collectionCellWidth)
+        let spacing = (UIScreen.main.bounds.width - collectionCellWidth - 40) / 2
+        layout.itemSize = CGSize(width: collectionCellWidth, height: collectionCellWidth)
+        layout.minimumInteritemSpacing = spacing
+        layout.minimumLineSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.layer.cornerRadius = 10
+        collectionView.isPagingEnabled = true
+        collectionView.decelerationRate = .fast
         collectionView.backgroundColor = ConstantColor.cellColor
         collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.identifier)
-        collectionView.contentInset = UIEdgeInsets(top: .zero, left: 10, bottom: .zero, right: 10)
         
         return collectionView
     }()
     
-    private let dateLabel = UILabel(text: "날짜", font: .preferredFont(forTextStyle: .body))
-    private let storeLabel = UILabel(text: "상호명", font: .preferredFont(forTextStyle: .body))
+    private let dateLabel = UILabel(text: "", font: .preferredFont(forTextStyle: .subheadline))
+    private let storeLabel = UILabel(text: "", font: .boldSystemFont(ofSize: 25))
     private let productLabel = UILabel(text: "내역", font: .preferredFont(forTextStyle: .body))
-    private let priceLabel = UILabel(text: "가격", font: .boldSystemFont(ofSize: 17))
-    private let countLabel = UILabel(text: "", font: .preferredFont(forTextStyle: .body))
+    private let priceLabel = UILabel(text: "가격", font: .boldSystemFont(ofSize: 20))
+    private let countLabel = UILabel(text: "", font: .preferredFont(forTextStyle: .caption1))
     
     private let payTypeSegmented: UISegmentedControl = {
         let segment = UISegmentedControl(items: ["현금", "카드"])
-        segment.isSelected = false
+        segment.isEnabled = false
         segment.selectedSegmentIndex = .zero
         segment.setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
         segment.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
@@ -49,7 +54,7 @@ final class DetailViewController: UIViewController, ViewModelBindable {
         let textView = UITextView()
         textView.isEditable = false
         textView.layer.cornerRadius = 10
-        textView.textColor = .lightGray
+        textView.textColor = .white
         textView.font = .preferredFont(forTextStyle: .body)
         textView.backgroundColor = ConstantColor.cellColor
         
@@ -65,11 +70,11 @@ final class DetailViewController: UIViewController, ViewModelBindable {
     )
     
     private lazy var mainStackView = UIStackView(
-        subViews: [dateLabel, storeLabel, productLabel, priceStackView, memoTextView],
+        subViews: [dateLabel, storeLabel, productLabel, priceStackView],
         axis: .vertical,
         alignment: .fill,
         distribution: .fill,
-        spacing: 20
+        spacing: 10
     )
     
     override func viewDidLoad() {
@@ -80,6 +85,8 @@ final class DetailViewController: UIViewController, ViewModelBindable {
     }
     
     func bindViewModel() {
+        changePageLabel(page: 1)
+        
         viewModel?.receiptData
             .bind(to: collectionView.rx.items(cellIdentifier: ImageCell.identifier, cellType: ImageCell.self)
             ) { indexPath, data, cell in
@@ -96,11 +103,56 @@ final class DetailViewController: UIViewController, ViewModelBindable {
         priceLabel.text = NumberFormatter.numberDecimal(from: viewModel?.receipt.price ?? .zero) + " 원"
         payTypeSegmented.selectedSegmentIndex = viewModel?.receipt.paymentType ?? .zero
         memoTextView.text = viewModel?.receipt.memo
+        
+        collectionView.rx.setDelegate(self)
+            .disposed(by: rx.disposeBag)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension DetailViewController: UICollectionViewDelegate {
+    private func changePageLabel(page: Int) {
+        let totalCount = (try? viewModel?.receiptData.value().count) ?? .zero
+        
+        countLabel.text = "\(page)/\(totalCount)"
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let visibleBounds = collectionView.bounds
+        let currentPage = Int(visibleBounds.midX / visibleBounds.width) + 1
+        
+        changePageLabel(page: currentPage)
+    }
+
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        let estimatedIndex = scrollView.contentOffset.x / cellWidthIncludingSpacing
+        
+        let tempCG: CGFloat
+        if velocity.x > .zero {
+            tempCG = ceil(estimatedIndex)
+        } else if velocity.x < .zero {
+            tempCG = floor(estimatedIndex)
+        } else {
+            tempCG = round(estimatedIndex)
+        }
+        
+        targetContentOffset.pointee = CGPoint(x: tempCG * cellWidthIncludingSpacing, y: .zero)
     }
 }
 
 extension DetailViewController {
     @objc func optionButtonTapped() {
+        
+    }
+    
+    @objc func composeButtonTapped() {
         
     }
 }
@@ -119,36 +171,60 @@ extension DetailViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationController?.navigationBar.backItem?.title = "뒤로가기"
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
+        let shareButton = UIBarButtonItem(
             image: UIImage(systemName: "square.and.arrow.up"),
             style: .plain,
             target: self,
             action: #selector(optionButtonTapped)
         )
+        
+        let composeButton = UIBarButtonItem(
+            image: UIImage(systemName: "ellipsis.circle"),
+            style: .plain,
+            target: self,
+            action: #selector(composeButtonTapped)
+        )
+        
+        navigationItem.rightBarButtonItems = [composeButton, shareButton]
     }
     
     private func setupView() {
         view.backgroundColor = ConstantColor.backGrouncColor
         priceLabel.textColor = ConstantColor.registerColor
-        [payTypeSegmented, memoTextView, collectionView].forEach {
+        dateLabel.textColor = .systemGray6
+        
+        mainStackView.backgroundColor = ConstantColor.cellColor
+        mainStackView.layer.cornerRadius = 10
+        mainStackView.layoutMargins = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+        mainStackView.isLayoutMarginsRelativeArrangement = true
+
+        [memoTextView, collectionView, memoTextView].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
-        [collectionView, mainStackView].forEach(view.addSubview(_:))
+        [mainStackView, collectionView, countLabel, memoTextView].forEach(view.addSubview(_:))
     }
     
     private func setupConstraints() {
         let safeArea = view.safeAreaLayoutGuide
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
-            collectionView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-            collectionView.widthAnchor.constraint(equalTo: safeArea.widthAnchor, multiplier: 0.7),
-            collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor),
-            
-            mainStackView.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 20),
+            mainStackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
             mainStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
             mainStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
-            mainStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20)
+            mainStackView.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.2),
+            
+            collectionView.topAnchor.constraint(equalTo: mainStackView.bottomAnchor, constant: 20),
+            collectionView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            collectionView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+            collectionView.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.3),
+
+            countLabel.topAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 5),
+            countLabel.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            
+            memoTextView.topAnchor.constraint(equalTo: countLabel.bottomAnchor, constant: 20),
+            memoTextView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            memoTextView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+            memoTextView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -30)
         ])
         
         priceLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
