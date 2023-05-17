a435f22d73e71e75823cd37b32102fde9281a08b
Kyo <hyosing92@gmail.com>
2023-05-17 15:05:16 +0900

    feat : 즐겨찾기 View, ViewModel 구현

--- ReceiptManager/ReceiptManager/Source/View/FavoriteScene/FavoriteListViewController.swift
+++ ReceiptManager/ReceiptManager/Source/View/FavoriteScene/FavoriteListViewController.swift
@@ -6,16 +6,130 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
-class FavoriteListViewController: UIViewController, ViewModelBindable {
-    
+final class FavoriteListViewController: UIViewController, ViewModelBindable {
     var viewModel: FavoriteListViewModel?
+    
+    private var tableView = UITableView(frame: .zero, style: .insetGrouped)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupView()
+        setupTableView()
+        setupConstraints()
     }
     
     func bindViewModel() {
+        guard let viewModel = viewModel else { return }
         
+        // ViewModel Properties Binding
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.receiptList
+            .bind(to: tableView.rx.items(dataSource: viewModel.dataSource))
+            .disposed(by: rx.disposeBag)
+        
+        tableView.rx.setDelegate(self)
+            .disposed(by: rx.disposeBag)
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension FavoriteListViewController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath
+    ) -> UISwipeActionsConfiguration? {
+        let favoriteAction = UIContextualAction(
+            style: .normal,
+            title: nil,
+            handler: { [weak self] _, _, completion in
+                self?.viewModel?.favoriteAction(indexPath: indexPath)
+                completion(true)
+            }
+        )
+        
+        let label = UILabel(text: "즐겨찾기 해제", font: .preferredFont(forTextStyle: .body))
+        label.textColor = ConstantColor.cellColor
+        label.backgroundColor = .systemYellow
+        label.sizeToFit()
+        
+        favoriteAction.backgroundColor = .systemYellow
+        favoriteAction.image = UIImage(view: label)
+        
+        let swipeActions = UISwipeActionsConfiguration(actions: [favoriteAction])
+        
+        return swipeActions
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let data = viewModel?.dataSource[section]
+        
+        let string = data?.identity
+        
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.textColor = .white
+        label.text = string
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let headerView = UITableViewHeaderFooterView(reuseIdentifier: "HeaderView")
+
+        headerView.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: headerView.contentView.leadingAnchor, constant: 15),
+            label.centerYAnchor.constraint(equalTo: headerView.centerYAnchor)
+        ])
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+}
+
+// MARK: - UIConstraint
+extension FavoriteListViewController {
+    private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = ConstantColor.backGrouncColor
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.backItem?.title = "뒤로가기"
+    }
+    
+    private func setupView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = ConstantColor.backGrouncColor
+    }
+    
+    private func setupTableView() {
+        tableView.backgroundColor = ConstantColor.backGrouncColor
+        tableView.register(ListTableViewCell.self, forCellReuseIdentifier: ListTableViewCell.identifier)
+    }
+    
+    private func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
     }
 }
--- ReceiptManager/ReceiptManager/Source/ViewModel/FavoriteListViewModel.swift
+++ ReceiptManager/ReceiptManager/Source/ViewModel/FavoriteListViewModel.swift
@@ -6,7 +6,53 @@
 //
 
 import Foundation
+import RxSwift
+import RxDataSources
+import RxCocoa
 
 final class FavoriteListViewModel: CommonViewModel {
+    typealias TableViewDataSource = RxTableViewSectionedAnimatedDataSource<ReceiptSectionModel>
     
+    private let disposeBag = DisposeBag()
+
+    var receiptList: Observable<[ReceiptSectionModel]> {
+        return storage.fetch(type: .month).map { receiptSectionModels in
+            return receiptSectionModels.map { model in
+                let filteredItems = model.items.filter { $0.isFavorite }
+                return ReceiptSectionModel(model: model.model, items: filteredItems)
+            }
+            .filter { !$0.items.isEmpty }
+        }
+    }
+    
+    let dataSource: TableViewDataSource = {
+        let dataSource = TableViewDataSource { dataSource, tableView, indexPath, receipt in
+            
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: ListTableViewCell.identifier, for: indexPath
+            ) as? ListTableViewCell else {
+                let cell = UITableViewCell()
+                return cell
+            }
+            
+            cell.setupData(data: receipt)
+            return cell
+        }
+        
+        dataSource.canEditRowAtIndexPath = { _, _ in return true }
+        
+        return dataSource
+    }()
+    
+    func favoriteAction(indexPath: IndexPath) {
+        receiptList
+            .take(1)
+            .map { $0[indexPath.section].items }
+            .subscribe(onNext: { [weak self] items in
+                var receipt = items[indexPath.row]
+                receipt.isFavorite.toggle()
+                self?.storage.upsert(receipt: receipt)
+            })
+            .disposed(by: disposeBag)
+    }
 }
