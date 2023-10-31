adf570023c007b88b1cda879dee4ebd4353c23a8
KyoPak <hyosing92@gmail.com>
2023-10-31 14:00:35 +0900

    refactor : 소수점 표기 로직 수정

--- ReceiptManager/ReceiptManager/Source/Utility/Extension/NumberFormatter+Extension.swift
+++ ReceiptManager/ReceiptManager/Source/Utility/Extension/NumberFormatter+Extension.swift
@@ -13,21 +13,24 @@ extension NumberFormatter {
     static func numberDecimal(from numberText: String) -> String {
         standard.numberStyle = .decimal
         
+        let texts = numberText.split(separator: ".").map { String($0) }
+        
+        guard texts.count != 0,
+            let integer = Int(texts[0]),
+            let integerText = standard.string(from: NSNumber(value: integer)) else {
+            return ""
+        }
+        
+        if texts.count == 2 {
+            return integerText + "." + String(texts[1])
+        }
+        
         let numbers = numberText.map { String($0) }
-        guard let doubleValue = Double(numberText), let lastNumber = numbers.last else { return "" }
         
-        if lastNumber == "." {
-            return (standard.string(from: NSNumber(value: Int(doubleValue))) ?? "") + "."
+        if texts.count == 1 && numbers.contains(".") {
+            return integerText + "."
         }
-        
-        if numbers.contains(".") && lastNumber == "0" {
-            return (standard.string(from: NSNumber(value: Int(doubleValue))) ?? "") + ".0"
-        }
-        
-        if doubleValue.truncatingRemainder(dividingBy: 1.0) == 0 {
-            return standard.string(from: NSNumber(value: Int(doubleValue))) ?? ""
-        } else {
-            return standard.string(from: NSNumber(floatLiteral: doubleValue)) ?? ""
-        }
+                
+        return integerText
     }
 }
