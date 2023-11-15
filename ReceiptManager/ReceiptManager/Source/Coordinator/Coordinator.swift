486c65a90a1ed7e12ce60359848d8c99e6dd3680
KyoPak <hyosing92@gmail.com>
2023-11-15 23:17:52 +0900

    feat : Coordinator 내부 removeChild, close 메서드 기본 구현 추가

--- ReceiptManager/ReceiptManager/Source/Coordinator/Coordinator.swift
+++ ReceiptManager/ReceiptManager/Source/Coordinator/Coordinator.swift
@@ -16,6 +16,28 @@ protocol Coordinator: AnyObject {
     
     func start()
     
-    func removeChild(to child: Coordinator?)
-    func close(to controller: UIViewController)
+    func removeChild(_ child: Coordinator?)
+    func close(_ controller: UIViewController)
+}
+
+extension Coordinator {
+    func removeChild(_ child: Coordinator?) {
+        guard let child = child,
+              let parentCoordinator = parentCoordinator else { return }
+        parentCoordinator.childCoordinators.removeAll(where: { $0 === child })
+    }
+    
+    func close(_ controller: UIViewController) {
+        var controllers = navigationController.viewControllers
+        if controller.parent == nil {
+            controller.dismiss(animated: true)
+            return
+        }
+        
+        removeChild(self)
+        controllers.removeAll(where: { $0 === controller })
+        
+        guard let lastController = controllers.last else { return }
+        navigationController.popToViewController(lastController, animated: true)
+    }
 }
