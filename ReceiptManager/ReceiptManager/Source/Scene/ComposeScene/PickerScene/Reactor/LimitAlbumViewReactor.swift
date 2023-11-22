ecda327d343da6cfc6f0c504d2e21c748926473c
KyoPak <hyosing92@gmail.com>
2023-11-22 19:03:15 +0900

    config : LimitAlbumViewReactor 파일 생성

---
+++ ReceiptManager/ReceiptManager/Source/Scene/ComposeScene/PickerScene/Reactor/LimitAlbumViewReactor.swift
@@ -0,0 +1,47 @@
+//
+//  LimitAlbumViewReactor.swift
+//  ReceiptManager
+//
+//  Created by parkhyo on 2023/11/22.
+//
+
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+final class LimitAlbumViewReactor: Reactor {
+    
+    // Reactor Properties
+    
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    let initialState = State()
+    
+    // Properties
+    
+    // Initializer
+    
+    init(title: String) {
+        
+    }
+    
+    // Reactor Method
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        
+    }
+}
