d4441a1eb1bdc9bf6a77609c2a7e05dc1c8475b7
KyoPak <hyosing92@gmail.com>
2023-11-23 23:28:11 +0900

    feat : SegmentDelegate 프로토콜 추가 및 SettingCell 코드 정리, currencySegmented 바인딩 구현

--- ReceiptManager/ReceiptManager/Source/Scene/SettingScene/SettingCell.swift
+++ ReceiptManager/ReceiptManager/Source/Scene/SettingScene/SettingCell.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
+import RxSwift
+
+@objc protocol SegmentDelegate: AnyObject {
+    @objc dynamic func segmentDelegate(index: Int)
+}
+
 final class SettingCell: UITableViewCell {
-    private let optionLabel = UILabel(font: .preferredFont(forTextStyle: .body))
-//    weak var delegate: SegmentDelegate?
     
-    let currencySegmented: UISegmentedControl = {
+    // Properties
+    
+    weak var delegate: SegmentDelegate?
+    private var disposeBag = DisposeBag()
+    
+    // UI Properties
+    
+    private let optionLabel = UILabel(font: .preferredFont(forTextStyle: .body))
+    
+    private let currencySegmented: UISegmentedControl = {
         let segment = UISegmentedControl(items: [ConstantText.krw, ConstantText.usd, ConstantText.jpy])
         segment.selectedSegmentIndex = .zero
         segment.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
@@ -23,10 +36,13 @@ final class SettingCell: UITableViewCell {
         return segment
     }()
     
+    // Initializer
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupView()
-        setupConstraint()
+        setupHierarchy()
+        setupProperties()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -37,39 +53,42 @@ final class SettingCell: UITableViewCell {
         super.prepareForReuse()
         currencySegmented.isHidden = true
     }
-}
-
-// MARK: - SegmentController Action
-extension SettingCell {
-    @objc private func changedCurrency() {
-
+    
+    private func bind() {
+        currencySegmented.rx.selectedSegmentIndex
+            .asDriver(onErrorJustReturn: .zero)
+            .drive { self.delegate?.segmentDelegate(index: $0) }
+            .disposed(by: disposeBag)
     }
 }
 
 // MARK: - UI Constraint & Setting UI Component
 extension SettingCell {
-    private func setupView() {
+    private func setupHierarchy() {
+        [optionLabel, currencySegmented].forEach(contentView.addSubview(_:))
+    }
+    
+    private func setupProperties() {
         backgroundColor = ConstantColor.cellColor
         layer.borderColor = ConstantColor.layerColor.cgColor
         layer.borderWidth = 1
         layer.cornerRadius = 5
-        [optionLabel, currencySegmented].forEach(contentView.addSubview(_:))
-        
-        currencySegmented.addTarget(self, action: #selector(changedCurrency), for: .valueChanged)
     }
     
     func setupData(text: String) {
         optionLabel.text = text
     }
     
-    func showSegment(index: Int) {
+    func setupSegment(index: Int) {
         currencySegmented.isHidden = false
         currencySegmented.selectedSegmentIndex = index
-        setupSegmentConstraint()
+        bind()
+        setupSegmentConstraints()
     }
     
-    private func setupSegmentConstraint() {
+    private func setupSegmentConstraints() {
         let safeArea = contentView.safeAreaLayoutGuide
+        
         NSLayoutConstraint.activate([
             currencySegmented.topAnchor.constraint(equalTo: optionLabel.topAnchor),
             currencySegmented.leadingAnchor.constraint(equalTo: optionLabel.trailingAnchor, constant: 10),
@@ -80,7 +99,7 @@ extension SettingCell {
         currencySegmented.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
     }
     
-    private func setupConstraint() {
+    private func setupConstraints() {
         let safeArea = contentView.safeAreaLayoutGuide
         NSLayoutConstraint.activate([
             optionLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
