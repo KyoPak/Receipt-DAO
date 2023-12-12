8d2889e17466b7a036c2b8341653df70a7f69a88
KyoPak <hyosing92@gmail.com>
2023-12-12 14:58:42 +0900

    refactor : OCRTextExtractor Class 로직 수정 및 불필요한 코드 삭제

--- ReceiptManager/ReceiptManager/Source/Utility/OCR/OCRTextExtractable.swift
+++ ReceiptManager/ReceiptManager/Source/Utility/OCR/OCRTextExtractable.swift
@@ -6,18 +6,18 @@
 //
 
 import Foundation
-import RxSwift
-import RxCocoa
 import Vision
 import VisionKit
 
+import RxSwift
+
 protocol OCRTextExtractable {
-    var ocrResult: PublishRelay<OCRResult> { get set }
+    var ocrResult: PublishSubject<[String]> { get set }
     func extractText(data: Data)
 }
 
 final class OCRTextExtractor: OCRTextExtractable {
-    var ocrResult = PublishRelay<OCRResult>()
+    var ocrResult = PublishSubject<[String]>()
     
     func extractText(data: Data) {
         guard let image = UIImage(data: data)?.cgImage else { return }
@@ -35,7 +35,7 @@ final class OCRTextExtractor: OCRTextExtractable {
                 text.topCandidates(1).first?.string.trimmingCharacters(in: .whitespacesAndNewlines)
             }
             
-            self.filterOCR(ocrText)
+            ocrResult.onNext(ocrText)
         }
         
         if #available(iOS 16.0, *) {
@@ -54,85 +54,5 @@ final class OCRTextExtractor: OCRTextExtractable {
         } catch {
             print(error)
         }
-    }
-    
-    private func filterOCR(_ result: [String]) {
-        var filterResult = OCRResult()
-        
-        // 상호명 Default
-        filterResult.store = result.first ?? ""
-        
-        for index in 0..<result.count {
-            let text = result[index]
-            
-            // 상호명 추출
-            if text == "상호명" && index + 1 < result.count {
-                filterResult.store = result[index + 1]
-                continue
-            }
-            
-            if let dateExtract = extractDate(from: text) {
-                filterResult.date = dateExtract
-            }
-            
-            if let priceExtract = extractPrice(from: text) {
-                filterResult.price = priceExtract
-            }
-            
-            if let payTypeExtract = extractPayType(from: text) {
-                filterResult.paymentType = payTypeExtract
-            }
-        }
-        
-        ocrResult.accept(filterResult)
-    }
-    
-    // 날짜 추출
-    private func extractDate(from text: String) -> Date? {
-        let dateFormats = [
-            "yyyy-MM-dd HH:mm:ss",
-            "yyyy/MM/dd HH:mm:ss",
-            "yy-MM-dd HH:mm:ss",
-            "yy/MM/dd HH:mm:ss",
-            "yyyy-MM-dd"
-        ]
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ko_KR") // 날짜 형식의 로케일 설정
-        
-        for format in dateFormats {
-            dateFormatter.dateFormat = format
-            if let date = dateFormatter.date(from: text) {
-                return date
-            }
-        }
-        
-        return nil
-    }
-    
-    // 가격 추출
-    private func extractPrice(from text: String) -> Int? {
-        if text.contains("원") || text.contains(".") || text.contains(",") {
-            let priceText = text.replacingOccurrences(of: "원", with: "")
-                .replacingOccurrences(of: ",", with: "")
-                .replacingOccurrences(of: ".", with: "")
-                .replacingOccurrences(of: " ", with: "")
-            
-            guard let price = Int(priceText) else { return nil }
-            return price
-        }
-        
-        return nil
-    }
-    
-    // 현금/카드 타입 추출
-    private func extractPayType(from text: String) -> Int? {
-        if text.contains(PayType.cash.description) {
-            return PayType.cash.rawValue
-        } else if text.contains(PayType.card.description) {
-            return PayType.card.rawValue
-        }
-        
-        return nil
     }
 }
