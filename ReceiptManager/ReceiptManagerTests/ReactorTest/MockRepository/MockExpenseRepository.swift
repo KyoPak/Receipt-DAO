2a4d94a6f7399c443cb6e2d24b813760e2a9fee1
KyoPak <hyosing92@gmail.com>
2024-01-17 16:30:14 +0900

    test : MockExpenseRepostory 내부 로직 수정

--- ReceiptManager/ReceiptManagerTests/ReactorTest/MockRepository/MockExpenseRepository.swift
+++ ReceiptManager/ReceiptManagerTests/ReactorTest/MockRepository/MockExpenseRepository.swift
@@ -10,21 +10,51 @@ import RxSwift
 
 final class MockExpenseRepository: ExpenseRepository {
     var saveEvent: PublishSubject<Receipt>
+    private var datas: [Receipt] = [Receipt(), Receipt(), Receipt()]
     
     init() {
         saveEvent = PublishSubject()
     }
     
     func fetchExpenses() -> Observable<[Receipt]> {
-        return Observable.just([Receipt(), Receipt(), Receipt()])
+        return Observable.just(datas)
     }
     
     func save(expense: Receipt) -> Observable<Receipt> {
+        var updateFlag = false
+        for data in datas {
+            if data.identity == expense.identity {
+                var index = datas.firstIndex(of: data)!
+                datas.remove(at: index)
+                datas.append(expense)
+                updateFlag = true
+            }
+        }
+        
+        if !updateFlag { datas.append(expense) }
+        
         saveEvent.onNext(expense)
         return Observable.just(expense)
     }
     
     func delete(expense: Receipt) -> Observable<Receipt> {
+        var deleteFlag = false
+        
+        for data in datas {
+            if data.identity == expense.identity {
+                var index = datas.firstIndex(of: data)!
+                datas.remove(at: index)
+                deleteFlag = true
+                break
+            }
+        }
+        
+        if !deleteFlag { return Observable.error(MockExpenseRepositoryError.deleteError) }
+        
         return Observable.just(expense)
     }
+}
+
+enum MockExpenseRepositoryError: Error {
+    case deleteError
 }
