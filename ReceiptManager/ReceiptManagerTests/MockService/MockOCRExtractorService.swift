952fd826afed55a177dc4617490ab7ad1cab7146
KyoPak <hyosing92@gmail.com>
2024-01-17 15:23:50 +0900

    test : MockOCRExtractorService 로직 수정 및 에러추가, OCRRepositoryTests Test 코드 작성

--- ReceiptManager/ReceiptManagerTests/MockService/MockOCRExtractorService.swift
+++ ReceiptManager/ReceiptManagerTests/MockService/MockOCRExtractorService.swift
@@ -10,9 +10,16 @@ import RxSwift
 
 final class MockOCRExtractorService: OCRExtractorService {
     var ocrResult = PublishSubject<[String]>()
+    var error: MockOCRExtractorError?
     
     func extract(data: Data) -> Observable<[String]> {
+        if error != nil { return Observable.error(error ?? .extractFail) }
+        
         ocrResult.onNext(["Test Text"])
         return ocrResult
     }
+}
+
+enum MockOCRExtractorError: Error {
+    case extractFail
 }
