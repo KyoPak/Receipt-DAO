7bd2963545255a560679f31876facee9a293b527
KyoPak <hyosing92@gmail.com>
2024-02-20 15:52:14 +0900

    feat : PaymentTypeRepository 선언 및 구체 타입 구현

---
+++ ReceiptManager/ReceiptManager/Source/Repository/PaymentTypeRepository.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentTypeRepository.swift
+//  ReceiptManager
+//
+//  Created by parkhyo on 2/17/24.
+//
+
+import Foundation
+
+protocol PaymentTypeRepository {
+    func fetchPaymentTypeIndex() -> Int
+    func updatePaymentType(index: Int) -> Int
+}
+
+final class DefaultPaymentTypeRepository: PaymentTypeRepository {
+    private let service: UserDefaultService
+    
+    init(service: UserDefaultService) {
+        self.service = service
+    }
+    
+    func fetchPaymentTypeIndex() -> Int {
+        return service.fetch(key: .payment)
+    }
+    
+    func updatePaymentType(index: Int) -> Int {
+        return service.update(key: .payment, index: index)
+    }
+}
