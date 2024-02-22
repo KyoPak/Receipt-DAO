da33554094de243529efe7baa946da5964163b54
KyoPak <hyosing92@gmail.com>
2024-02-22 16:28:02 +0900

    test : MockUserDefaultService, MockUserSettingRepository 수정

---
+++ ReceiptManager/ReceiptManagerTests/ReactorTest/MockRepository/MockUserSettingRepository.swift
@@ -0,0 +1,28 @@
+//
+//  MockUserSettingRepository.swift
+//  ReceiptManagerTests
+//
+//  Created by parkhyo on 1/17/24.
+//
+
+import RxSwift
+@testable import ReceiptManager
+
+final class MockUserSettingRepository: UserSettingRepository {
+    private var dict: [OptionKeyType: Int] = [.currency: .zero, .payment: .zero, .displayMode: .zero]
+    
+    var currencyChangeEvent: BehaviorSubject<Int>
+    
+    init() {
+        currencyChangeEvent = BehaviorSubject(value: .zero)
+    }
+    
+    func fetchIndex(type: OptionKeyType) -> Int {
+        return dict[type]!
+    }
+    
+    func updateIndex(type: OptionKeyType, index: Int) -> Int {
+        dict[type] = index
+        return dict[type]!
+    }
+}
