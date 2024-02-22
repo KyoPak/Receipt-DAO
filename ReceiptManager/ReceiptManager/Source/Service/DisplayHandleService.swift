44cd0b1833728c1fb4052b6fd6aa49f81bb9522b
KyoPak <hyosing92@gmail.com>
2024-02-22 14:19:18 +0900

    feat : DisplayHandleService 프로토콜 정의 및 구체 타입 구현

---
+++ ReceiptManager/ReceiptManager/Source/Service/DisplayHandleService.swift
@@ -0,0 +1,54 @@
+//
+//  DisplayHandleService.swift
+//  ReceiptManager
+//
+//  Created by parkhyo on 2/22/24.
+//
+
+import UIKit
+
+// Notificate Extension For DisplayMode
+
+extension Notification.Name {
+    static let displayModeNotification = Notification.Name("displayModeNotification")
+}
+
+protocol DisplayHandleService {
+    func applyDisplayMode(index: Int)
+}
+
+final class DefaultDisplayHandleService: DisplayHandleService {
+    
+    init() {
+        setupDisplayChangeObserver()
+    }
+    
+    private func setupDisplayChangeObserver() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(displayModeDidChange),
+            name: .displayModeNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func displayModeDidChange(_ notification: Notification) {
+        guard let index = notification.userInfo?[ConstantText.displayModeKey] as? Int else { return }
+        
+        applyDisplayMode(index: index)
+    }
+    
+    func applyDisplayMode(index: Int) {
+        let style: UIUserInterfaceStyle = {
+            switch DisplayMode(rawValue: index) {
+            case .light:    return .light
+            case .dark:     return .dark
+            default:        return .unspecified
+            }
+        }()
+        
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = style
+        }
+    }
+}
